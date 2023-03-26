bcdbc96535873849b22dbcbe130dd36e19caeddd
SebastianKurrle <kurrlesebasitan@gmail.com>
2023-03-26 16:15:39 +0200

    Added function to create superuser

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email adress')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name='Admin', password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email=email, name=name, password=password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -11,3 +32,5 @@ class User(AbstractUser):
     # email is username
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
