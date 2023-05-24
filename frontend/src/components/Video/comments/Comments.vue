0821ea884eabcbb5cfcfb53e9ed1e1768db66e5a
SebastianKurrle <kurrlesebasitan@gmail.com>
2023-05-24 19:23:19 +0200

    Implementet function wich checks the scroll traffic from the user

--- frontend/src/components/Video/comments/Comments.vue
+++ frontend/src/components/Video/comments/Comments.vue
@@ -1,17 +1,90 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, onMounted, watch } from 'vue'
+  import { useRoute } from "vue-router";
 
   // components
   import CommentPostInput from "@/components/Video/comments/CommentPostInput.vue";
 
+  const route = useRoute()
+
   const props = defineProps(['video'])
 
   const video = ref(props.video)
+
+  const maxScrollPos = ref(window.scrollY)
+
+  const updateMaxScrollPos = () => {
+    if (maxScrollPos.value < window.scrollY) {
+      maxScrollPos.value = window.scrollY
+    }
+  }
+
+  onMounted(() => {
+    window.addEventListener('scroll', () => {
+      if (route.name === 'video') {
+        updateMaxScrollPos()
+      }
+    })
+  })
 </script>
 
 <template>
   <div>
     <CommentPostInput :video="video"/>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
   </div>
 </template>
 
