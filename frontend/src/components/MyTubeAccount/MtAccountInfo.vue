eff9f761af4d3e417d723f674570885cd6ddf566
SebastianKurrle <kurrlesebasitan@gmail.com>
2023-06-04 01:08:09 +0200

    Implemented to display the count of subscribers on the frontend

--- frontend/src/components/MyTubeAccount/MtAccountInfo.vue
+++ frontend/src/components/MyTubeAccount/MtAccountInfo.vue
@@ -15,6 +15,10 @@
     const subscribed = ref()
     subscribed.value = false
 
+    const subCount = ref()
+
+    const loaded = ref(false)
+
     const checkSubscribed = async () => {
       subscribed.value = await subscribeStore.checkUserSubscribed(mtaccount.value.id)
     }
@@ -26,6 +30,7 @@
 
       await subscribeStore.subscribe(mtaccount.value.id)
       await checkSubscribed()
+      subCount.value = await subscribeStore.countSubscribers(mtaccount.value.id)
     }
 
     const unsubscribe = async () => {
@@ -35,18 +40,22 @@
 
       await subscribeStore.unsubscribe(mtaccount.value.id)
       await checkSubscribed()
+      subCount.value = await subscribeStore.countSubscribers(mtaccount.value.id)
     }
 
-    onMounted(() => {
+    onMounted(async () => {
       if (authenticatedStore.authenticated) {
         checkSubscribed()
       }
+
+      subCount.value = await subscribeStore.countSubscribers(mtaccount.value.id)
+      loaded.value = true
     })
 
 </script>
 
 <template>
-    <div class="mt-3 text-white">
+    <div class="mt-3 text-white" v-if="loaded">
         <div class="box">
             <div class="content">
                 <div class="w-36 h-36 rounded-full overflow-hidden">
@@ -54,24 +63,25 @@
                 </div>
                 <div class="ml-4">
                     <h2 class="text-3xl font-bold">{{ mtaccount.name }}</h2>
-                    <p class="text-lg text-gray-500">{{ mtaccount.description }}</p> 
+                    <p class="text-lg text-gray-500">{{ mtaccount.description }}</p>
+                    <p class="text-gray-300">{{ subCount }} Subscribers</p>
                 </div>
             </div>
     
             <div class="content">
-                <button class="bg-subutton2 p-3 rounded-3xl text-white font-semibold hover:bg-gray-600"
-                v-if="subscribed"
-                @click="unsubscribe"
-                >
-                  Subscribed
-                </button>
+              <button class="bg-subutton2 p-3 rounded-3xl text-white font-semibold hover:bg-gray-600"
+                      v-if="subscribed"
+                      @click="unsubscribe"
+              >
+                Subscribed
+              </button>
 
-                <button class="bg-subbutton p-3 rounded-3xl text-black font-semibold hover:bg-gray-300"
-                v-else
-                @click="subscribe"
-                >
-                    Subscribe
-                </button>
+              <button class="bg-subbutton p-3 rounded-3xl text-black font-semibold hover:bg-gray-300"
+                      v-else
+                      @click="subscribe"
+              >
+                Subscribe
+              </button>
             </div>
         </div>
 
--- frontend/src/stores/subscribeStore.ts
+++ frontend/src/stores/subscribeStore.ts
@@ -62,9 +62,42 @@ export const useSubscribeStore = defineStore('subscribe', () => {
             })
     }
 
+    /*
+        This function return the count of subscribers from a MyTube account
+    */
+    const countSubscribers = async (mtAccountID:string):Promise<string> => {
+        let subCount:string = ''
+
+        await axios
+            .get(`/api/mytube-account/subscribe/${mtAccountID}/`)
+            .then(response => {
+                subCount = formatNumber(response.data.subCount)
+            })
+            .catch(error => {
+                toast.error('Something went wrong', { autoClose: 3000 })
+            })
+
+        return subCount
+    }
+
+    /*
+        The function get an input number if the number is greater than one million
+        It cuts the number and ats the suffix Mio otherwise it do nothing
+        For example: 130000 => 1.30 Mio, 50 => 50
+    */
+    const formatNumber = (num:number):string => {
+        if (num >= 1000000) {
+            let shortNum = num / 1000000
+            return `${shortNum.toFixed(2)} Mio`
+        }
+
+        return String(num)
+    }
+
     return {
         checkUserSubscribed,
         subscribe,
-        unsubscribe
+        unsubscribe,
+        countSubscribers
     }
 })
